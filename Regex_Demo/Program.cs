4dca0542a3911c253ea95026b4be3eec4729eb2e
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-24 21:16:32 +0530

    [chiru] Check LastName

--- Regex_Demo/Program.cs
+++ Regex_Demo/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem");
-            Console.WriteLine("Enter 1 for First Name");
+            Console.WriteLine("Enter 1 for First Name\nEnter 2 for Last Name");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
@@ -14,8 +14,14 @@
                     Console.WriteLine("Enter a FirstName");
                     string FirstName = Console.ReadLine();
                     obj.Name(FirstName);
-                   
+                break;
+                case 2: //Last Name
+                    UC2_LastName CheckLastName = new UC2_LastName();
+                    Console.WriteLine("Enter a LastName");
+                    string LastName = Console.ReadLine();
+                    CheckLastName.Name(LastName);
                     break;
+
                 default:
                     Console.WriteLine("End Of the Programme");
                     break;
---
+++ Regex_Demo/UC2_LastName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    internal class UC2_LastName
+    {
+        public void Name(string name)
+        {
+            const String Pattern = "^[A-Z]{1}[a-z]{2,}[ ][A-Z]{1}[a-z]{2,}$";
+            if (Regex.IsMatch(name, Pattern))
+            {
+                Console.WriteLine(name + " is a valid Name");
+
+            }
+            else
+            {
+                Console.WriteLine(name + " is a invalid Name");
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
