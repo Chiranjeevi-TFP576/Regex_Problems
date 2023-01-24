221fe8bfe09b442644b90e288061754f1c861ee5
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-24 21:02:07 +0530

    [chiru] Checking FirstName

---
+++ Regex_Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Regex_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem");
+            Console.WriteLine("Enter 1 for First Name");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1: //First Name
+                    RegexCheck obj = new RegexCheck();
+                    Console.WriteLine("Enter a FirstName");
+                    string FirstName = Console.ReadLine();
+                    obj.Name(FirstName);
+                   
+                    break;
+                default:
+                    Console.WriteLine("End Of the Programme");
+                    break;
+
+            }
+        }
+    }
+}
