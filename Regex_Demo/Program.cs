012f9f6200cac46115780e771eb55314ec408154
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-25 10:27:07 +0530

    [chiru] Add Give AtLeast OneNumber

--- Regex_Demo/Program.cs
+++ Regex_Demo/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to User Registration Problem");
 
-            Console.WriteLine("Enter 1 for First Name\nEnter 2 for Last Name\nEnter 3 for Email\nEnter 4 for MobileNumber\nEnter 5 for PassWord\nEnter 6 for PassWord AtLeast One UPPerCase");
+            Console.WriteLine("Enter 1 for First Name\nEnter 2 for Last Name\nEnter 3 for Email\nEnter 4 for MobileNumber\nEnter 5 for PassWord\nEnter 6 for PassWord AtLeast One UPPerCase\n\nEnter 7 for PassWord AtLeast One Number");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
@@ -45,6 +45,12 @@
                     Console.WriteLine("Enter a PassWord");
                     string UpperCasePassWord = Console.ReadLine();
                     Password.Name(UpperCasePassWord);
+                    break;
+                case 7: //PassWord At Least One Number
+                    UC7_AtLeastOne_Number Password1 = new UC7_AtLeastOne_Number();
+                    Console.WriteLine("Enter a PassWord");
+                    string OneNumber = Console.ReadLine();
+                    Password1.Name(OneNumber);
                     break;
                 default:
                     Console.WriteLine("End Of the Programme");
---
+++ Regex_Demo/UC7_AtLeastOne Number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Demo
+{
+    internal class UC7_AtLeastOne_Number
+    {
+        public void Name(string name)
+        {
+            const String Pattern = "^[A-Z]+[a-zA-Z]{6,16}[0-9]+$";
+            if (Regex.IsMatch(name, Pattern))
+            {
+                Console.WriteLine(name + " is a valid PassWord");
+
+            }
+            else
+            {
+                Console.WriteLine(name + " is a invalid PassWord");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
